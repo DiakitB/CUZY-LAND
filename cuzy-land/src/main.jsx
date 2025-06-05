17d2f43c3b9f1d44e99347a86f4c5c0f6e77ab96
DiakitB <babacar.diakite01@gmail.com>
2025-06-05 17:14:11 -0400

    trying to deploy cuzy-land

--- cuzy-land/src/main.jsx
+++ cuzy-land/src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {CartProvider} from './context/cartContext'
+// import { CartProvider } from './context/cartContext';
+import { CartProvider } from './context/cartContext.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   <CartProvider>
